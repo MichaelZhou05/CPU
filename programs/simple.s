620cb62b6c253c737da9072c43d954214b2bc09e
Fletch Rydell <fletch.rydell@duke.edu>
2025-10-23 20:51:09 -0400

    kit

---
+++ programs/simple.s
@@ -0,0 +1,19 @@
+.text
+    # just set each register to its number (r1=1, r2=2, etc.)
+    addi $r1, $r0, 1
+    addi $r2, $r0, 2
+    addi $r3, $r0, 3
+    addi $r4, $r0, 4
+    addi $r5, $r0, 5
+    addi $r6, $r0, 6
+    addi $r7, $r0, 7
+
+    # a few no-ops to pad out the tester runtime
+    addi $r0, $r0, 0
+    addi $r0, $r0, 0
+    addi $r0, $r0, 0
+    addi $r0, $r0, 0
+    addi $r0, $r0, 0
+    addi $r0, $r0, 0
+
+    halt
